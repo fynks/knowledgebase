b354b3592de77c4970a74d367029d428c962e949
fynks <75840152+fynks@users.noreply.github.com>
2025-01-26 19:53:11 +0500

    simplified icons

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -8,12 +8,9 @@ export default defineConfig({
   cleanUrls: true,
 
   head: [
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' }],
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
-    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: 'images/apple-touch-icon.png' }],
     ['meta', { name: 'apple-mobile-web-app-title', content: 'KnowledgeBase' }],
-    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
   ],
 
   themeConfig: {
